58e82d857d44bdb7d04ad3c504b6d3b3834ade15
Jaime Simeón Palomar Blumenthal <alu0101228587@ull.edu.es>
2022-12-26 15:25:15 +0000

    getArrayImgs()

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <pokeCard msg="Welcome to Your Vue.js App"/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import pokeCard from './components/pokeCard.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    pokeCard
   }
 }
 </script>
